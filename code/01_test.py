878abb182b0677137a15affe942b205e4a54f13c
pineapple-big <55078444+pineapple-big@users.noreply.github.com>
2020-04-19 13:18:16 +0800

    Update 01_test.py

--- code/01_test.py
+++ code/01_test.py
@@ -5,6 +5,7 @@ driver = webdriver.Chrome()
 driver.get("https://www.baidu.com/")
 driver.maximize_window()
 driver.implicitly_wait(30)
+driver.find_element_by_id("username").send_keys("admin")
 
 time.sleep(3)
 driver.quit()
