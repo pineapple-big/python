7da17ea29f13120e59a638df54a58682e8686343
big pineapple <1171515510@qq.com>
2020-04-19 13:22:47 +0800

    添加了代码

--- code/01_test.py
+++ code/01_test.py
@@ -6,6 +6,7 @@ driver.get("https://www.baidu.com/")
 driver.maximize_window()
 driver.implicitly_wait(30)
 driver.find_element_by_id("username").send_keys("admin")
+driver.find_element_by_id("password").send_keys("123456")
 
 time.sleep(3)
 driver.quit()
